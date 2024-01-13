56f493ea9b96aa93f48d33a9b610c390eb391393
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-14 01:37:22 +0600

    first commit

---
+++ components/navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 border-b px-10 py-5">
+      <div className="flex justify-between ">
+        <div>
+            <p>
+            NavBat
+            </p>
+        </div>
+        <div>
+            <p>
+                menu
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+
